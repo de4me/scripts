7069aad0870c534d86aa349686757387d8fe1966
Denis Popov <info@de4me.com>
2025-03-02 20:01:59 +0300

    Don't remove DTB files;

--- Repka/compress-u-boot.sh
+++ Repka/compress-u-boot.sh
@@ -13,25 +13,12 @@ echo "process: $UBOOTROOT"
 
 cd ~/
 mkdir u-boot-repka-pi3/
+
 cd u-boot-repka-pi3/
 mkdir dtb/ bootloader/
 
 cp $UBOOTROOT/u-boot-sunxi-with-spl.bin bootloader/
 cp $UBOOTROOT/arch/arm/dts/*repka*.dtb dtb/
-
-cd dtb/
-
-if [ -f "sun50i-h5-repka-pi3-allwinner.dtb" ]; then
-  rm sun50i-h5-repka-pi3-allwinner.dtb
-fi
-
-if [ -f "sun50i-h5-repka-pi3.dtb" ]; then
-  rm sun50i-h5-repka-pi3.dtb
-fi
-
-if [ -f "sun50i-h5-repka-pi3-alt.dtb" ]; then
-  rm sun50i-h5-repka-pi3-alt.dtb
-fi
 
 cd ~/
 rm u-boot-repka-pi3.tar.gz
