14506e000c586bcd4c607a835b4461e436674f4e
Denis Popov <info@de4me.com>
2025-11-03 17:47:08 +0000

    repka: Added support for dts overlays;

--- Repka/install-u-boot-orangepi.sh
+++ Repka/install-u-boot-orangepi.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # Repka bootloader writer
-# version: 2025-07-21
+# version: 2025-08-26
 
 
 if [ -z "$HOST" ]; then
@@ -28,6 +28,45 @@ if [ -z "$DEVICEROOT" ]; then
   export DEVICEROOT=u-boot-repka-pi3
 fi
 
+#
+# VARIABLES
+#
+
+if [ "$1" == "v" ]; then
+  echo "HOST=$HOST"
+  echo "HOSTUSER=$HOSTUSER"
+  echo "HOSTSOURCE=$HOSTSOURCE"
+  echo "DEVICELOADER=$DEVICELOADER"
+  echo "DEVICETREE=$DEVICETREE"
+  echo "DEVICEROOT=$DEVICEROOT"
+  exit 0
+fi
+
+#
+# RESTORE
+#
+
+if [ "$1" == "restore" ]; then
+  if [ -f "/boot/u-boot-sunxi-with-spl.bin.bak" ]; then
+    echo "* restore: /boot/u-boot-sunxi-with-spl.bin.bak"
+    sudo dd if=/boot/u-boot-sunxi-with-spl.bin.bak of=/dev/mmcblk0 bs=1024 seek=8
+  fi
+  if [ -f "/boot/orangepiEnv.txt.bak" ]; then
+    echo "* restore: /boot/orangepiEnv.txt"
+    sudo rm -f /boot/orangepiEnv.txt
+    sudo cp -f /boot/orangepiEnv.txt.bak /boot/orangepiEnv.txt 
+  fi
+  if [ -d "/boot/dtb/allwinner/overlay.bak" ]; then
+    echo "* restore: /boot/dtb/allwinner/overlay"
+    sudo rm -rf /boot/dtb/allwinner/overlay/ 
+    sudo cp -rf /boot/dtb/allwinner/overlay.bak/ /boot/dtb/allwinner/overlay/ 
+  fi
+  exit 0
+fi
+
+#
+# DOWNLOAD / UNPACK
+#
 
 if [ -n "$1" ]; then
   if [ ! -f "$1" ]; then
@@ -57,6 +96,15 @@ if [ ! -d "$DEVICEROOT" ]; then
   exit
 fi
 
+#
+# BACKUP
+#
+
+if [ ! -f "/boot/u-boot-sunxi-with-spl.bin.bak" ]; then
+  echo "** backup: /boot/u-boot-sunxi-with-spl.bin.bak"
+  sudo dd if=/dev/mmcblk0 of=/boot/u-boot-sunxi-with-spl.bin.bak bs=1024 skip=8 count=1017
+fi
+
 if [ ! -f "/boot/orangepiEnv.txt.bak" ]; then
   echo "** backup: /boot/orangepiEnv.txt"
   sudo cp /boot/orangepiEnv.txt /boot/orangepiEnv.txt.bak
@@ -67,14 +115,17 @@ fi
 if [ -d "/boot/dtb/allwinner/overlay" ]; then
   if [ ! -d "/boot/dtb/allwinner/overlay.bak" ]; then
     echo "** backup: /boot/dtb/allwinner/overlay"
-    sudo cp -rf "/boot/dtb/allwinner/overlay/" "/boot/dtb/allwinner/overlay.bak/"
+    sudo cp -rf /boot/dtb/allwinner/overlay/ /boot/dtb/allwinner/overlay.bak/
   fi
-  sudo rm /boot/dtb/allwinner/overlay/sun50i-h5-*.dtbo
 fi
+
+#
+# WRITE
+#
 
 if [ -f "$DEVICEROOT/bootloader/$DEVICELOADER" ]; then
   echo "*** write: $DEVICEROOT/bootloader/$DEVICELOADER"
-  sudo dd if=$DEVICEROOT/bootloader/$DEVICELOADER of=/dev/mmcblk0 bs=1024 seek=8 conv=notrunc
+  sudo dd if=$DEVICEROOT/bootloader/$DEVICELOADER of=/dev/mmcblk0 bs=1024 seek=8
 fi
 
 if [ -f "$DEVICEROOT/dtb/$DEVICETREE" ]; then
@@ -82,9 +133,11 @@ if [ -f "$DEVICEROOT/dtb/$DEVICETREE" ]; then
   sudo cp -vf $DEVICEROOT/dtb/$DEVICETREE /boot/dtb/allwinner/
 fi
 
-echo "**** write: overlays"
-
-for file in $DEVICEROOT/dtb/*.dtbo; do
-  filename=$(basename ${file} | sed s/repka-pi3-overlay-//)
-  sudo cp -vf "$file" "/boot/dtb/allwinner/overlay/$filename"
-done
+if [ -d "/boot/dtb/allwinner/overlay" ]; then
+  echo "*** write: overlays"
+  sudo rm -f /boot/dtb/allwinner/overlay/sun50i-h5-*.dtbo
+  for file in $DEVICEROOT/dtb/*.dtbo; do
+    filename=$(basename ${file} | sed s/repka-pi3-overlay-//)
+    sudo cp -vf "$file" "/boot/dtb/allwinner/overlay/$filename"
+  done
+fi
--- Repka/install-u-boot-ubuntu.sh
+++ Repka/install-u-boot-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # Repka bootloader writer
-# version: 2015-02-15
+# version: 2025-07-20
 
 
 if [ -z "$HOST" ]; then
@@ -21,13 +21,52 @@ if [ -z "$DEVICELOADER" ]; then
 fi
 
 if [ -z "$DEVICETREE" ]; then
-  export DEVICETREE=sun50i-h5-repka-pi3-v1.4.dtb
+  export DEVICETREE=sun50i-h5-repka-pi3-1ghz.dtb
 fi
 
 if [ -z "$DEVICEROOT" ]; then
   export DEVICEROOT=u-boot-repka-pi3
 fi
 
+#
+# VARIABLES
+#
+
+if [ "$1" == "v" ]; then
+  echo "HOST=$HOST"
+  echo "HOSTUSER=$HOSTUSER"
+  echo "HOSTSOURCE=$HOSTSOURCE"
+  echo "DEVICELOADER=$DEVICELOADER"
+  echo "DEVICETREE=$DEVICETREE"
+  echo "DEVICEROOT=$DEVICEROOT"
+  exit 0
+fi
+
+#
+# RESTORE
+#
+
+if [ "$1" == "restore" ]; then
+  if [ -f "/boot/u-boot-sunxi-with-spl.bin.bak" ]; then
+    echo "* restore: /boot/u-boot-sunxi-with-spl.bin.bak"
+    sudo dd if=/boot/u-boot-sunxi-with-spl.bin.bak of=/dev/mmcblk0 bs=1024 seek=8
+  fi
+  if [ -f "/boot/repka-pi.dtb.bak" ]; then
+    echo "* restore: /boot/repka-pi.dtb.bak"
+    sudo rm -f "/boot/repka-pi.dtb"
+    sudo cp -fv "/boot/repka-pi.dtb.bak" "/boot/repka-pi.dtb"
+  fi
+  if [ -d "/boot/overlays.bak" ]; then
+    echo "* restore: /boot/overlays.bak"
+    sudo rm -rf "/boot/overlays/"
+    sudo cp -rfv "/boot/overlays.bak" "/boot/overlays"
+  fi
+  exit 0
+fi
+
+#
+# DOWNLOAD / UNPACK
+#
 
 if [ -n "$1" ]; then
   if [ ! -f "$1" ]; then
@@ -57,13 +96,13 @@ if [ ! -d "$DEVICEROOT" ]; then
   exit
 fi
 
-if [ -f "/boot/repkaEnv.txt" ]; then
-  if [ -f "/boot/repkaEnv.txt.bak" ]; then
-    sudo rm -f "/boot/repkaEnv.txt"
-  else
-    echo "** backup: /boot/repkaEnv.txt"
-    sudo mv -f "/boot/repkaEnv.txt" "/boot/repkaEnv.txt.bak"
-  fi
+#
+# BACKUP
+#
+
+if [ ! -f "/boot/u-boot-sunxi-with-spl.bin.bak" ]; then
+  echo "** backup: /boot/u-boot-sunxi-with-spl.bin.bak"
+  sudo dd if=/dev/mmcblk0 of=/boot/u-boot-sunxi-with-spl.bin.bak bs=1024 skip=8 count=1017
 fi
 
 if [ ! -f "/boot/repka-pi.dtb.bak" ]; then
@@ -71,9 +110,18 @@ if [ ! -f "/boot/repka-pi.dtb.bak" ]; then
   sudo mv -f "/boot/repka-pi.dtb" "/boot/repka-pi.dtb.bak"
 fi
 
+if [ ! -d "/boot/overlays.bak" ]; then
+  echo "** backup: /boot/overlays/"
+  sudo cp -rf "/boot/overlays/" "/boot/overlays.bak/"
+fi
+
+#
+# WRITE
+#
+
 if [ -f "$DEVICEROOT/bootloader/$DEVICELOADER" ]; then
   echo "*** write: $DEVICEROOT/bootloader/$DEVICELOADER"
-  sudo dd if=$DEVICEROOT/bootloader/$DEVICELOADER of=/dev/mmcblk0 bs=1024 seek=8 conv=notrunc
+  sudo dd if=$DEVICEROOT/bootloader/$DEVICELOADER of=/dev/mmcblk0 bs=1024 seek=8
 fi
 
 if [ -f "$DEVICEROOT/dtb/$DEVICETREE" ]; then
@@ -81,3 +129,11 @@ if [ -f "$DEVICEROOT/dtb/$DEVICETREE" ]; then
   sudo cp -vf $DEVICEROOT/dtb/$DEVICETREE /boot/repka-pi.dtb
 fi
 
+if [ -d "/boot/overlays/" ]; then
+  echo "*** write: overlays"
+  sudo rm -f "/boot/overlays/*.dtbo"
+  for file in $DEVICEROOT/dtb/*.dtbo; do
+    filename=$(basename ${file} | sed s/sun50i-h5-repka-pi3-overlay-//)
+    sudo cp -vf "$file" "/boot/overlays/$filename"
+  done
+fi
